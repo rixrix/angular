65052d4133826e7010bf65e3ddad2f2b32d32d31
Richard Sentino <richard@mindginative.com>
2015-09-07 17:32:05 +1200

    wip work

--- modules/angular2/test_lib.ts
+++ modules/angular2/test_lib.ts
@@ -2,3 +2,7 @@
 export * from './test';
 export * from './src/test_lib/utils';
 export * from './src/test_lib/fake_async';
+export * from './src/http/backends/mock_backend';
+export * from './src/http/interfaces';
+export * from './src/http/enums';
+export * from './src/http/static_request';
